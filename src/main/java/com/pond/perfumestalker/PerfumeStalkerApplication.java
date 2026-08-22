9b6c7f20966af5ab70eaff77ff5af8f5033d53a7
jshjshjshjsh <cj456456@gmail.com>
2026-08-22 23:25:48 +0900

    first commit

---
+++ src/main/java/com/pond/perfumestalker/PerfumeStalkerApplication.java
@@ -0,0 +1,13 @@
+package com.pond.perfumestalker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfumeStalkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerfumeStalkerApplication.class, args);
+    }
+
+}
